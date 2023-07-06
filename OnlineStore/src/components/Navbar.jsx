3767212cfc10e396d1745cfcf51a23ac53d8ac4d
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-07-06 09:10:34 +0545

    ui changes for Navbar

--- OnlineStore/src/components/Navbar.jsx
+++ OnlineStore/src/components/Navbar.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-600 w-full h-7 flex justify-between text-white">
-      <Link to="/">Online Store</Link>
-      <Link to="/search">Search</Link>
+    <nav className="bg-[#21222a] items-center w-full mb-10 font-bold font-serif h-16 flex justify-between text-slate-200">
+      <Link className="ml-6 hover:text-[#80ceff] text-2xl" to="/">
+        Online Store
+      </Link>
+      <p className="font-mono text-lg">Welcome to OnlineStore.</p>
+      <Link className="mr-6 text-xl hover:text-[#80ceff]" to="/search">
+        Search
+      </Link>
     </nav>
   );
 };
